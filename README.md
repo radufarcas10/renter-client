15f2a1c4d53fa183f3fc2f3f9f02281714a40e05
Radu Farcas <radu.farcas@evozon.com>
2017-10-12 17:11:19 +0300

    configured webpack for development

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # renter-client
 Repo for Renter client application
+
+```git clone https://github.com/radufarcas10/renter-client.git
+cd renter-client
+npm start```
---
+++ webpack.dev.config.js
@@ -0,0 +1,81 @@
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const OfflinePlugin = require('offline-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve('src/'),
+  devtool: 'eval',
+  entry: [
+    'react-hot-loader/patch',
+    './index.js',
+  ],
+  output: {
+    filename: 'js/[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'css-hot-loader' },
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer({ browsers: ['> 1%', 'IE >= 10'] })],
+            },
+          },
+          { loader: 'sass-loader' },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({ template: '../public/index.html' }),
+    // new webpack.IgnorePlugin(/\.svg$/),
+    // new OfflinePlugin({ caches: { main: [] } }),
+  ],
+  devServer: {
+    hot: true,
+    inline: true,
+    open: true,
+    port: '8080',
+    // It suppress error shown in console, so it has to be set to false.
+    // quiet: false,
+    // It suppress everything except error, so it has to be set to false as well
+    // to see success build.
+    // noInfo: false,
+    stats: {
+      hash: true,           // add the hash of the compilation
+      version: false,       // add webpack version information
+      timings: true,        // add timing information
+      assets: false,        // add assets information
+      chunks: true,         // add chunk information
+      chunkModules: false,  // add built modules information to chunk information
+      modules: false,       // add built modules information
+      cached: true,         // add also information about cached (not built) modules
+      reasons: true,        // add information about the reasons why modules are included
+      source: false,        // add the source code of modules
+      errorDetails: false,  // add details to errors (like resolving log)
+      chunkOrigins: false,  // add the origins of chunks and chunk merging info
+      modulesSort: false,   // (string) sort the modules by that field
+      chunksSort: false,    // (string) sort the chunks by that field
+      assetsSort: false,
+    }
+  },
+};
